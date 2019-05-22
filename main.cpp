2b42b3422bd11fe743444d40541825def8e381ed
Paul Giesel <s-pagies@frontend.service>
2019-05-22 13:51:49 +0200

    Das Programm liest jetzt eine input.txt und schreibt dann die in konvertierten werte in eine output.txt

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-double fahrenheit(double celsius)
-{
+double fahrenheit(double celsius){
   return celsius*9./5.+32;
 }
 
-int main()
-{
+//double celsius(double fahrenheit){
+//  return fahrenheit....
+
+int main(){
+  vector<double> inputvector;
+  std::ifstream ga("input.txt");
   double var1;
-  ifstream in("input.txt");
-  in>>var1;
-  double Wert = fahrenheit(var1);
-  cout << Wert << endl;
-  return 0;
+
+  for(int i=0; i<3; i++){
+    ga>>var1;
+    inputvector.push_back(var1);
+  }
+
+  vector<double> outputvector;
+  double var2;
+
+  for(int i=0; i<inputvector.size(); i++){
+    double var2 = fahrenheit(inputvector[i]);
+    outputvector.push_back(var2);
+  }
+
+  ofstream out("output.txt");
+
+  for(int i=0; i<outputvector.size(); i++){
+    out << outputvector[i] << endl;
+  }
+
+  return 0; 
 }
