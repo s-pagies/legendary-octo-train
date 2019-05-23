b6b77536e863db46f51c8a3685eb4d0dc7b82c7d
Paul Giesel <s-pagies@frontend.service>
2019-05-23 09:28:20 +0200

    Dokumentation hinzugefügt

--- main.cpp
+++ main.cpp
@@ -11,25 +11,26 @@ double fahrenheit(double celsius){
 //  return fahrenheit....
 
 int main(){
+
+  //liest input.txt in inputvector ein
   vector<double> inputvector;
   std::ifstream ga("input.txt");
   double var1;
-
   for(int i=0; i<3; i++){
     ga>>var1;
     inputvector.push_back(var1);
   }
-
+  
+  //wendet Celsius-Fahrenheit Formel auf inputvector an und schreibt Ergebnis in outputvector
   vector<double> outputvector;
   double var2;
-
   for(int i=0; i<inputvector.size(); i++){
     double var2 = fahrenheit(inputvector[i]);
     outputvector.push_back(var2);
   }
 
+  //writes outputvector into output.txt on harddrive
   ofstream out("output.txt");
-
   for(int i=0; i<outputvector.size(); i++){
     out << outputvector[i] << endl;
   }
